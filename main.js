c8b04e3723a6598197a63d5a54b58a03f4ce6835
Zong Zheng Li <contact@zongzhengli.com>
2017-02-12 21:01:55 -0500

    Remove unused ko binding

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 $(function() {
-    ko.bindingHandlers.fadeVisible = {
-        init: function(element, valueAccessor) {
-            var value = valueAccessor();
-            $(element).toggle(ko.unwrap(value));
-        },
-        update: function(element, valueAccessor) {
-            var value = valueAccessor();
-            ko.unwrap(value) ? $(element).fadeIn() : $(element).fadeOut();
-        }
-    };
-    
     $(".popover").popover();
 
     app = new ViewModel();

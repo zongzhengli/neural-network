a021ded12428773df2bbf0ab623bff39c9517817
Zong Zheng Li <contact@zongzhengli.com>
2018-07-29 09:50:14 -0400

    Add help button
    - Add help button and popover to explain how things work
    - Rename forget button to reset

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 $(function() {
     $('[data-toggle="popover"]').popover();
 
+    $('.popover-dismiss').popover({
+      trigger: 'focus',
+    });
+
     app = new App();
     app.drawAll();
     ko.applyBindings(app);
